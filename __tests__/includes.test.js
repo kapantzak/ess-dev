2404184c6289910181d3eb8fa060f70a887fdb0c
kapantzak <i.kapantzakis@epsilonnet.gr>
2020-02-18 13:46:57 +0200

    Adds ts compile includes tests

--- __tests__/includes.test.js
+++ __tests__/includes.test.js
@@ -1,30 +1,118 @@
 const includes = require("../lib/includes");
 
-describe("getUserControlInclusionCompile()", () => {
-  test("Returns the expected items for the provided user control", () => {
-    const actual = includes.getUserControlInclusionCompile("ucTestForm");
-    const expected = [
-      {
-        _attributes: {
-          Include: `ucTestForm.ascx.cs`
+describe("Compile", () => {
+  describe("getUserControlInclusionCompile()", () => {
+    test("Returns the expected items for the provided user control", () => {
+      const actual = includes.getUserControlInclusionCompile("ucTestForm");
+      const expected = [
+        {
+          _attributes: {
+            Include: `ucTestForm.ascx.cs`
+          },
+          DependentUpon: {
+            _text: `ucTestForm.ascx`
+          },
+          SubType: {
+            _text: "ASPXCodeBehind"
+          }
         },
-        DependentUpon: {
-          _text: `ucTestForm.ascx`
-        },
-        SubType: {
-          _text: "ASPXCodeBehind"
+        {
+          _attributes: {
+            Include: `ucTestForm.ascx.designer.cs`
+          },
+          DependentUpon: {
+            _text: `ucTestForm.ascx`
+          }
         }
-      },
-      {
-        _attributes: {
-          Include: `ucTestForm.ascx.designer.cs`
-        },
-        DependentUpon: {
-          _text: `ucTestForm.ascx`
+      ];
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("getAsyncHandlerInclusionCompile()", () => {
+    test("Returns the expected items for the provided user control", () => {
+      const actual = includes.getAsyncHandlerInclusionCompile("ucTestForm");
+      const expected = [
+        {
+          _attributes: {
+            Include: `Async\\testForm.ashx.cs`
+          },
+          DependentUpon: {
+            _text: `testForm.ashx`
+          }
         }
-      }
-    ];
-    expect(actual).toEqual(expected);
+      ];
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("getModelsInclusionCompile()", () => {
+    test("Returns the expected items for the provided user control", () => {
+      const actual = includes.getModelsInclusionCompile("ucTestForm");
+      const expected = [
+        {
+          _attributes: {
+            Include: `classes\\HttpRequestsDataModels\\TestForm\\State.cs`
+          }
+        },
+        {
+          _attributes: {
+            Include: `classes\\HttpRequestsDataModels\\TestForm\\Filters.cs`
+          }
+        },
+        {
+          _attributes: {
+            Include: `classes\\HttpRequestsDataModels\\TestForm\\RequestParams.cs`
+          }
+        },
+        {
+          _attributes: {
+            Include: `classes\\HttpRequestsDataModels\\TestForm\\Results.cs`
+          }
+        }
+      ];
+      expect(actual).toEqual(expected);
+    });
+  });
+});
+
+describe("Typescript Compile", () => {
+  describe("getPageScriptInclusionTsCompile()", () => {
+    test("Returns the expected items for the provided user control", () => {
+      const actual = includes.getPageScriptInclusionTsCompile("ucTestForm");
+      const expected = [
+        {
+          _attributes: {
+            Include: `src\\pageScripts\\testForm.ts`
+          }
+        }
+      ];
+      expect(actual).toEqual(expected);
+    });
+
+    test("Returns the expected items for the provided user control", () => {
+      const actual = includes.getAsyncHelperInclusionTsCompile("ucTestForm");
+      const expected = [
+        {
+          _attributes: {
+            Include: `src\\asyncHelpers\\asyncTestForm.ts`
+          }
+        }
+      ];
+      expect(actual).toEqual(expected);
+    });
+
+    test("Returns the expected items for the provided user control", () => {
+      const actual = includes.getStateHelperInclusionTsCompile("ucTestForm");
+      const expected = [
+        {
+          _attributes: {
+            Include: `src\\reduxStates\\state_testForm.ts`
+          }
+        }
+      ];
+      expect(actual).toEqual(expected);
+    });
   });
 });
 
