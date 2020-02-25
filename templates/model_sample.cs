7d3d90a7efd5410c80b9732faa73a0a83d627b52
kapantzak <i.kapantzakis@epsilonnet.gr>
2020-02-25 17:23:08 +0200

    Adds model_sample.cs template

---
+++ templates/model_sample.cs
@@ -0,0 +1,14 @@
+using eStudio.classes.CustomAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eStudio.classes.HttpRequestsDataModels.{{name}}
+{
+    [ExportToTypescript]
+    public class {{name}}
+    {
+           
+    }
+}
